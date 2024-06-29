5c40d3160eb4e32f4207d9486b8159629eb0aeab
zmai2014 <170424485+zmai2014@users.noreply.github.com>
2024-06-29 11:26:44 +0000

    Initial Commit4

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'books' => 'books#new'
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  get 'books' => 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  root to: 'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
