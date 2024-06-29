448f762edbe42d00675c5dc74624c33fb60372f4
zmai2014 <170424485+zmai2014@users.noreply.github.com>
2024-06-29 16:29:10 +0000

    Initial Commit6

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,8 +25,12 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
